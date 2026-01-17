db3be9f201da03fefab908daa5c66ce0523a44eb
Isabele Torres <isabeledasilvatorres@gmail.com>
2026-01-17 11:53:58 -0300

    Creat Project

---
+++ src/test/java/com/torres/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torres.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
