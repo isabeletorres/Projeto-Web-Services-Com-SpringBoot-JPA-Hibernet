2801e22dc32edc6c4ad5b5cd86ac1ee598f003ab
Isabele Torres <isabeledasilvatorres@gmail.com>
2026-01-29 12:03:22 -0300

    Product

--- src/main/java/com/torres/project/entities/Category.java
+++ src/main/java/com/torres/project/entities/Category.java
@@ -1,9 +1,12 @@
 package com.torres.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name ="tb_category")
@@ -15,6 +18,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
     private String nome;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category(){}
 
@@ -39,6 +46,10 @@ public class Category implements Serializable {
         this.nome = nome;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -50,4 +61,5 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 }
---
+++ src/main/java/com/torres/project/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.torres.project.services;
+
+import com.torres.project.entities.Product;
+import com.torres.project.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
