167e5788b1af967ca0f988330cb46e30f9b55fd9
Isabele Torres <isabeledasilvatorres@gmail.com>
2026-01-29 08:58:14 -0300

    Order, Insatant, ISO 8601 -PART 1.

--- src/main/java/com/torres/project/entities/User.java
+++ src/main/java/com/torres/project/entities/User.java
@@ -1,7 +1,10 @@
 package com.torres.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,6 +21,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User(){}
 
     public User(Long id, String nome, String email, String phone, String password) {
@@ -26,6 +33,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
