22011daebeeac90f9d030baedeb4e15ac5d9bb02
Isabele Torres <isabeledasilvatorres@gmail.com>
2026-01-29 11:28:19 -0300

    Category

--- src/main/java/com/torres/project/config/TestConfig.java
+++ src/main/java/com/torres/project/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.torres.project.config;
 
+import com.torres.project.entities.Category;
 import com.torres.project.entities.Order;
 import com.torres.project.entities.User;
 import com.torres.project.entities.enums.OrderStatus;
+import com.torres.project.repositories.CategoryRepository;
 import com.torres.project.repositories.OrderRepository;
 import com.torres.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,16 @@ public class TestConfig  implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -33,5 +43,6 @@ public class TestConfig  implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
     }
 }
