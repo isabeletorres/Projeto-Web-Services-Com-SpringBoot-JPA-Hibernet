f49ab109d752b6876664d39203c89111a056d7a2
Isabele Torres <isabeledasilvatorres@gmail.com>
2026-01-30 19:14:21 -0300

    User Insert

--- src/main/java/com/torres/project/entities/User.java
+++ src/main/java/com/torres/project/entities/User.java
@@ -14,7 +14,7 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
-    private String nome;
+    private String name;
     private String email;
     private String phone;
     private String password;
@@ -25,9 +25,9 @@ public class User implements Serializable {
 
     public User(){}
 
-    public User(Long id, String nome, String email, String phone, String password) {
+    public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
-        this.nome = nome;
+        this.name = name;
         this.email = email;
         this.phone = phone;
         this.password = password;
@@ -45,12 +45,12 @@ public class User implements Serializable {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String nome) {
+        this.name = nome;
     }
 
     public String getPhone() {
@@ -73,8 +73,8 @@ public class User implements Serializable {
         return password;
     }
 
-    public void setPassword(String assword) {
-        this.password = assword;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
